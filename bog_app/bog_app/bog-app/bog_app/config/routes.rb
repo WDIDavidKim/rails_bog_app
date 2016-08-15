73f8e966855b60d972848a706d7307126b2115e3
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-14 20:34:57 -0700

    get, create new, and post done

--- bog_app/bog_app/bog-app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog-app/bog_app/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new"
+  post "creatures", to: "creatures#create", as: "creature"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
