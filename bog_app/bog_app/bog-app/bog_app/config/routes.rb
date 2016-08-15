fe6da22739a8c7f8fc297b3367afb6c922e00a1f
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-14 21:00:41 -0700

    delete button and routes/method work

--- bog_app/bog_app/bog-app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog-app/bog_app/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
   get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
-  patch "creatures/:id", to: "creatures#update"
+  patch "/creatures/:id", to: "creatures#update"
+  delete "/creatures/:id", to: "creatures#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
