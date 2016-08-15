259354857874dfd6575862a1ea8c7f7c1595ff50
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-14 20:09:19 -0700

    show index for fourth attempt made

---
+++ bog_app/bog_app/bog-app/bog_app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root "creatures#index"
+
+  get "/creatures", to: "creatures#index", as: "creatures"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
