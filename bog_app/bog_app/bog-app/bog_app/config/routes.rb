bfa5f82498ee825c91670645a7434308d9ea3184
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-14 20:47:10 -0700

    show is now working

--- bog_app/bog_app/bog-app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog-app/bog_app/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures", to: "creatures#index", as: "creatures"
-  get "/creatures/new", to: "creatures#new"
-  post "creatures", to: "creatures#create", as: "creature"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
